5d64e53ce4688aa621a221f6063988f342a911ee
Jules Fouchy <jules.fouchy@ntymail.com>
2024-11-22 01:43:42 +0100

    ✅ Update tests to new API

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -6,7 +6,10 @@
 auto main(int argc, char* argv[]) -> int
 {
     {
-        no_sleep::Scoped _scoped{"Test"};
+        no_sleep::Scoped scoped{"MyApp", "MyApp is exporting a video", no_sleep::Mode::KeepScreenOnAndKeepComputing};
+    }
+    {
+        no_sleep::Scoped scoped{"MyApp", "MyApp is doing some long computation", no_sleep::Mode::ScreenCanTurnOffButKeepComputing};
     }
     bool const should_run_imgui_tests = argc < 2 || strcmp(argv[1], "-nogpu") != 0; // NOLINT(*pointer-arithmetic)
     if (!should_run_imgui_tests)
@@ -15,26 +18,30 @@ auto main(int argc, char* argv[]) -> int
     std::optional<no_sleep::Scoped> scope1{};
     std::optional<no_sleep::Scoped> scope2{};
 
+    quick_imgui::AverageTime average_time{};
+
     quick_imgui::loop("no_sleep tests", [&]() { // Open a window and run all the ImGui-related code
         ImGui::Begin("no_sleep tests");
+        average_time.stop();
+        average_time.start();
         bool sleep_not_allowed = scope1.has_value();
-        if (ImGui::Checkbox("Sleep not allowed", &sleep_not_allowed))
+        if (ImGui::Checkbox("Keep screen on and keep computing", &sleep_not_allowed))
         {
             if (sleep_not_allowed)
-                scope1.emplace("MyApp is exporting a video");
+                scope1.emplace("MyApp", "MyApp is exporting a video", no_sleep::Mode::KeepScreenOnAndKeepComputing);
             else
                 scope1.reset();
         }
         bool sleep_not_allowed2 = scope2.has_value();
-        if (ImGui::Checkbox("Sleep not allowed2", &sleep_not_allowed2))
+        if (ImGui::Checkbox("Screen can turn off but keep computing", &sleep_not_allowed2))
         {
             if (sleep_not_allowed2)
-                scope2.emplace("MyApp is exporting a video");
+                scope2.emplace("MyApp", "MyApp is doing some long computation", no_sleep::Mode::ScreenCanTurnOffButKeepComputing);
             else
                 scope2.reset();
         }
+        average_time.imgui_plot();
         ImGui::End();
-        ImGui::ShowDemoWindow();
     });
 
     return 0;
